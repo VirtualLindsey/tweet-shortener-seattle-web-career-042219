93cb25281551afa9e5e19baf6f2506bacf8a4222
L D <tapircode@gmail.com>
2019-04-10 06:08:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,17 +1,17 @@
 
 def dictionary()
   d = {
-    "hello" => "hi",
-    "to"=>"2",
-    "too"=> "2",
-    "two" => "2",
-    "for"=> "4",
-    "four"=> "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@", 
-    "and" => "&"
-}
+        "hello" => "hi",
+        "to"=>"2",
+        "too"=> "2",
+        "two" => "2",
+        "for"=> "4",
+        "four"=> "4",
+        "be" => "b",
+        "you" => "u",
+        "at" => "@", 
+        "and" => "&"
+      }
   return d
 end
 
